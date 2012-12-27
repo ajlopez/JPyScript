fae71a28ac4d28c9a30ce0f3150070897d86d242
ajlopez <webmaster@ajlopez.com>
2012-12-27 20:58:20 -0300

    Parse integer, test

---
+++ test/parser.js
@@ -0,0 +1,17 @@
+
+var py2s = require('../'),
+    assert = require('assert');
+
+assert.ok(py2s.Parser);
+
+function parseExpression(text) {
+    var parser = new py2s.Parser(text);
+    var expr = parser.parseExpression();
+    assert.ok(expr);
+    assert.equal(parser.parseExpression(), null);
+    return expr;
+}
+
+// Parse integer expression
+
+parseExpression("123");
