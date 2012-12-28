0c98b3df07f75f875925e8542105ec461ecd9c5f
ajlopez <webmaster@ajlopez.com>
2012-12-28 05:54:11 -0300

    Evaluate expression tests

---
+++ test/evalexpr.js
@@ -0,0 +1,21 @@
+
+var py2s = require('../'),
+    assert = require('assert');
+
+assert.ok(py2s.Parser);
+
+function evaluateExpression(text) {
+    var parser = new py2s.Parser(text);
+    var expr = parser.parseExpression();
+    assert.ok(expr);
+    assert.equal(parser.parseExpression(), null);
+    return eval(expr.compile());
+}
+
+// Evaluate integer
+
+assert.equal(evaluateExpression("123"), 123);
+
+// Evaluate string
+
+assert.equal(evaluateExpression('"spam"'), "spam");
