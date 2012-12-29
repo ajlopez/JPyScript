436c5dadf2745d90015ab8cfdf338a1cc75197f9
ajlopez <webmaster@ajlopez.com>
2012-12-29 14:28:12 -0300

    Runtime with len, eval

---
+++ test/eval.js
@@ -0,0 +1,9 @@
+
+var py2s = require('../'),
+    assert = require('assert');
+
+assert.ok(py2s.eval);
+
+assert.equal(py2s.eval('123'), 123);
+assert.equal(py2s.eval('"spam"'), "spam");
+assert.equal(py2s.eval('len("spam")'), 4);
