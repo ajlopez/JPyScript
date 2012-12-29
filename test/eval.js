62db56447fc1b8cdd48f62d1321c27c30b2dab20
ajlopez <webmaster@ajlopez.com>
2012-12-29 14:59:09 -0300

    Negative index in []

--- test/eval.js
+++ test/eval.js
@@ -4,11 +4,20 @@ var py2s = require('../'),
 
 assert.ok(py2s.eval);
 
+// Simple
+
 assert.equal(py2s.eval('123'), 123);
+assert.equal(py2s.eval('-123'), -123);
 assert.equal(py2s.eval('"spam"'), "spam");
+
+// len
+
 assert.equal(py2s.eval('len("spam")'), 4);
 
 // Index
 
 assert.equal(py2s.eval('"spam"[0]'), 's');
+assert.equal(py2s.eval('"spam"[1]'), 'p');
+assert.equal(py2s.eval('"spam"[-1]'), 'm');
+assert.equal(py2s.eval('"spam"[-2]'), 'a');
 
