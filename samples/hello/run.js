36ec315dd7aeb8184d7a7afb3b6c3320eacdce7f
ajlopez <webmaster@ajlopez.com>
2012-12-29 18:47:51 -0300

    Internal print function; hello sample refactores

--- samples/hello/run.js
+++ samples/hello/run.js
@@ -1,29 +1,14 @@
 
 var py2script = require('../..'),
-    util = require('util'),
     fs = require('fs');
     
-function compileFileCommand(filename) {
+function executeFile(filename) {
     var text = fs.readFileSync(filename).toString();
-    var parser = new py2script.Parser(text);
-    var cmd = parser.parseCommand();
-    if (!cmd)
-        return '';
-    return cmd.compile();
+    py2script.execute(text);
 };
-
-function print() {
-    var n = arguments.length;
-    
-    for (var k = 0; k < n; k++)
-        if (arguments[k] !== null)
-            util.print(arguments[k]);
-            
-    util.print('\n');
-}
 
 process.argv.forEach(function(val) {
     if (val.slice(-3) == ".py")
-        eval(compileFileCommand(val));
+        executeFile(val);
 });
 
