553e4c32533e566b2c4236b8e6e5e5dd14a89e1a
ajlopez <webmaster@ajlopez.com>
2012-12-29 19:21:57 -0300

    Refactor while.py in simple sample

--- samples/simple/while.py
+++ samples/simple/while.py
@@ -1,7 +1,12 @@
+
+print('-----------')
+
 n = 1
 total = 1
-while n <= 10:
-  total *= n
-  n += n
 
-print(total)
+while n <= 10:
+  print(n, '! = ', total)
+  total *= n
+  n += 1
+
+print('-----------')
