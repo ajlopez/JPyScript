dc681a79e6d96c5afce6ac3dfc690793d3723116
ajlopez <webmaster@ajlopez.com>
2013-01-01 17:29:29 -0300

    Execute while test

--- test/execute.js
+++ test/execute.js
@@ -20,3 +20,10 @@ py2s.execute("acum = ''; for letter in 'spam':\n  acum += letter\n  acum += '.'"
 assert.equal(py2s.evaluate('acum'), 's.p.a.m.');
 assert.ok(acum);
 assert.equal(acum, 's.p.a.m.');
+
+// while
+
+py2s.execute('total = 0; while total < 10: total += 1');
+assert.equal(py2s.evaluate('total'), 10);
+assert.ok(total);
+assert.equal(total, 10);
