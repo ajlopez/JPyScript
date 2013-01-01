54e2f1918b76b4d3c2cba05b862cc3c4f8e0ee63
ajlopez <webmaster@ajlopez.com>
2013-01-01 20:00:09 -0300

    def with arguments, skipping new lines, compile tests

--- test/compile.js
+++ test/compile.js
@@ -205,3 +205,5 @@ assert.equal(compileCommand('def f(): print(1)'), 'function f() { print(1); }');
 assert.equal(compileCommand('def f():\n  print(1)\n  print(2)'), 'function f() { print(1); print(2); }');
 assert.equal(compileCommand('def f(a):\n  print(a)'), 'function f(a) { print(a); }');
 assert.equal(compileCommand('def f(a,b):\n  print(a)\n  print(b)'), 'function f(a, b) { print(a); print(b); }');
+assert.equal(compileCommand('def f(\na\n):\n  print(a)'), 'function f(a) { print(a); }');
+assert.equal(compileCommand('def f(\na,\nb\r\n):\n  print(a)\n  print(b)'), 'function f(a, b) { print(a); print(b); }');
