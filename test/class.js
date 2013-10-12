f8d7687641919d35b6d0b56aeaf8b7c16759b860
ajlopez <webmaster@ajlopez.com>
2013-10-12 20:28:11 -0300

    Compile empty class

---
+++ test/class.js
@@ -0,0 +1,15 @@
+
+var jpyparser = require('../lib/jpyparser');
+
+function compileCommand(test, text) {
+    var parser = jpyparser.createParser(text);
+    var cmd = parser.parseCommand();
+    test.ok(cmd);
+    test.equal(parser.parseCommand(), null);
+    var code = cmd.compile(true);
+    return code;
+}
+
+exports['Compile empty class'] = function (test) {
+    test.equal(compileCommand(test, 'class Foo: pass'), 'function Foo() {  }');
+}
