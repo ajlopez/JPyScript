8df7d6f71afd3130f3340905e260cb7785ec04d9
ajlopez <webmaster@ajlopez.com>
2013-10-18 19:04:15 -0300

    Refactor, redesign execute

--- test/execute.js
+++ test/execute.js
@@ -2,24 +2,24 @@
 var py2s = require('../');
 
 exports['Simple'] = function (test) {
-    py2s.execute('a = 1');
+    py2s.execute('a = 1', true);
     test.equal(py2s.evaluate('a'), 1);
 }
 
 exports['for in'] = function (test) {
-    py2s.execute('total = 0; for item in [1,2,3]: total += item');
+    py2s.execute('total = 0; for item in [1,2,3]: total += item', true);
     test.equal(py2s.evaluate('total'), 6);
     test.ok(total);
     test.equal(total, 6);
 
-    py2s.execute("acum = ''; for letter in 'spam':\n  acum += letter\n  acum += '.'");
+    py2s.execute("acum = ''; for letter in 'spam':\n  acum += letter\n  acum += '.'", true);
     test.equal(py2s.evaluate('acum'), 's.p.a.m.');
     test.ok(acum);
     test.equal(acum, 's.p.a.m.');
 }
 
 exports['while'] = function (test) {
-    py2s.execute('total = 0; while total < 10: total += 1');
+    py2s.execute('total = 0; while total < 10: total += 1', true);
     test.equal(py2s.evaluate('total'), 10);
     test.ok(total);
     test.equal(total, 10);
