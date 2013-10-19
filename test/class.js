ba024cb54658fd401a964891de4867519ded5255
ajlopez <webmaster@ajlopez.com>
2013-10-19 17:48:28 -0300

    Compile class with __init__ and method

--- test/class.js
+++ test/class.js
@@ -14,5 +14,19 @@ exports['Compile empty class'] = function (test) {
     var code = compileCommand(test, 'class Foo: pass');
     test.ok(code.indexOf('var Foo = (function() {') == 0);
     test.ok(code.indexOf('return $cons;') >= 0);
-    test.ok(code.indexOf('makeClass(__init__);') >= 0);
+    test.ok(code.indexOf('var $class = makeClass(__init__);') >= 0);
+}
+
+exports['Compile class with init and method'] = function (test) {
+    var code = compileCommand(test, 'class Foo:\n  def __init__(self, name):\n    self.name=name\n  def get_name(self):\n     return self.name');
+    test.ok(code.indexOf('var Foo = (function() {') == 0);
+    test.ok(code.indexOf('return $cons;') >= 0);
+    test.ok(code.indexOf('var $class = makeClass(__init__);') >= 0);
+    test.ok(code.indexOf('function __init__(self, name)') >= 0);
+    test.ok(code.indexOf('function get_name(self)') >= 0);
+    test.ok(code.indexOf('self.name = name;') >= 0);
+    test.ok(code.indexOf('return self.name;') >= 0);
+    test.ok(code.indexOf('$cons.get_name = get_name;') >= 0);
+    test.ok(code.indexOf('$obj.prototype.get_name = function () { return get_name(this); }') >= 0);
+    test.ok(code.indexOf('function __init__()') < 0);
 }
