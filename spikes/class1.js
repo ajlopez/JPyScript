55fd23533bf41dc1249d0c77abb7a83d4920745d
ajlopez <webmaster@ajlopez.com>
2013-10-19 12:38:52 -0300

    __init__ in class

--- spikes/class1.js
+++ spikes/class1.js
@@ -9,7 +9,18 @@ var Foo = (function () {
             $obj.prototype.__class__ = Foo;
             
         var newobj = new $obj();
+        var initargs = [];
+        initargs[0] = newobj;
+        
+        for (var k = 0; k < arguments.length; k++)
+            initargs[k + 1] = arguments[k];
+        
+        __init__.apply(null, initargs);
         return newobj;
+    }
+    
+    function __init__(self, name) {
+        self.name = name;
     }
     
     function get_name(self) {
@@ -22,14 +33,12 @@ var Foo = (function () {
     return $cons;
 })();
 
-var obj = Foo();
+var obj = Foo('bar');
 
 assert.ok(obj);
 assert.equal(typeof obj, 'object');
 
 assert.equal(obj.__class__, Foo);
-
-obj.name = 'bar';
 
 assert.ok(obj.get_name);
 assert.equal(typeof obj.get_name, 'function');
