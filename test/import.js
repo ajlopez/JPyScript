0161896e59435317f0381720026d39ab42a4174c
ajlopez <webmaster@ajlopez.com>
2013-10-20 14:55:25 -0300

    Import local file

---
+++ test/import.js
@@ -0,0 +1,37 @@
+
+var py2s = require('../');
+
+exports['import math module'] = function (test) {
+    py2s.execute('import math', true);
+    var result = py2s.evaluate('math');
+    test.ok(result);
+    test.ok(result.PI);
+    test.ok(result.sin);
+    test.ok(result.cos);
+    test.ok(result.exp);
+    test.ok(result.abs);
+    test.equal(typeof result.sin, 'function');
+    test.equal(typeof result.cos, 'function');
+    test.equal(typeof result.exp, 'function');
+    test.equal(typeof result.abs, 'function');
+}
+
+exports['import http module'] = function (test) {
+    py2s.execute('import http', true);
+    var result = py2s.evaluate('http');
+    var myhttp = require('http');
+    test.ok(result);
+    test.strictEqual(result, myhttp);
+}
+
+exports['import local file'] = function (test) {
+    var original = process.cwd();
+    process.chdir(__dirname);
+    py2s.execute('import local', true);
+    process.chdir(original);
+    var result = py2s.evaluate('local');
+    test.ok(result);
+    test.equal(result.a, 1);
+    test.equal(result.b, 2);
+    test.equal(result.c, 3);
+}
---
+++ test/local.py
@@ -0,0 +1,7 @@
+
+# Local file to be imported by import.js test
+
+a = 1
+b = 2
+c = 3
+
