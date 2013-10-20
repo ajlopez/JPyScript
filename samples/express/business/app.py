16b97ec61120e1dee33de8d497f4077277c62683
ajlopez <webmaster@ajlopez.com>
2013-10-20 15:15:32 -0300

    Express business example, WIP

---
+++ samples/express/business/app.py
@@ -0,0 +1,52 @@
+
+# Module dependencies
+
+import express
+import path
+import http
+
+app = express()
+
+app.engine('ejs', require('ejs-locals'))
+
+# all environments
+
+app.set('port', process.env.PORT || 3000)
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.bodyParser())
+app.use(express.methodOverride())
+app.use(express.cookieParser('your secret here'))
+app.use(express.session())
+app.use(app.router)
+app.use(express.static('./public'))
+
+# development only
+if 'development' == app.get('env'):
+    app.use(express.errorHandler())
+    
+def index(request, response):
+    response.render('index', { 'title': 'My Business' })
+
+def about(request, response):
+    response.render('index', { 'title': 'About' })
+
+def contact(request, response):
+    response.render('index', { 'title': 'Contact' })
+
+def customer(request, response):
+    response.render('index', { 'title': 'Customers' })
+
+app.get('/', index)
+app.get('/about', about)
+app.get('/customer', customer)
+app.get('/contact', contact)
+
+def started():
+    console.log('Express server listening on port ' + app.get('port'))
+
+server = http.createServer(app)
+server.listen(app.get('port'), started)
+
---
+++ samples/express/business/run.js
@@ -0,0 +1,16 @@
+
+var jpyscript = require('../../..'),
+    fs = require('fs');
+    
+jpyscript.configure({ require: require });
+    
+function executeFile(filename) {
+    var text = fs.readFileSync(filename).toString();
+    jpyscript.execute(text);
+};
+
+process.argv.forEach(function(val) {
+    if (val.slice(-3) == ".py")
+        executeFile(val);
+});
+
